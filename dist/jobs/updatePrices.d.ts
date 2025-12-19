51855193f23521c877236e7613f7ddcdf74d9482
Rafik <rafik11111@yahoo.com>
2025-12-19 15:44:04 +0200

    build

--- dist/jobs/updatePrices.d.ts
+++ dist/jobs/updatePrices.d.ts
@@ -1,6 +1,6 @@
-export declare function updateGoldPrice(): Promise<import("mongoose").Document<unknown, {}, import("../models/metal.js").IMetal, {}, {}> & import("../models/metal.js").IMetal & Required<{
+export declare function updateGoldPrice(): Promise<(import("mongoose").Document<unknown, {}, import("../models/metal.js").IMetal, {}, {}> & import("../models/metal.js").IMetal & Required<{
     _id: unknown;
 }> & {
     __v: number;
-}>;
+}) | undefined>;
 //# sourceMappingURL=updatePrices.d.ts.map
--- dist/jobs/updatePrices.js
+++ dist/jobs/updatePrices.js
@@ -16,39 +16,50 @@ export async function updateGoldPrice() {
     if (!keys.keys || keys.keys.length === 0) {
         throw new Error("No API keys available in the keys document");
     }
-    const response = await axios.get(process.env.METAL_API_URL, {
-        headers: {
-            'x-access-token': keys.keys[keys.current]
+    try {
+        const response = await axios.get(process.env.METAL_API_URL, {
+            headers: {
+                'x-access-token': keys.keys[keys.current]
+            }
+        });
+        const data = response.data;
+        // Rotate keys safely
+        const keysLength = keys.keys.length;
+        const newCurrent = (keys.current + 1) % keysLength;
+        keys.current = newCurrent;
+        await keys.save();
+        const result = await Metal.findOneAndUpdate({ symbol: data.symbol }, {
+            timestamp: data.timestamp,
+            metal: data.metal,
+            currency: data.currency,
+            exchange: data.exchange,
+            symbol: data.symbol,
+            open_time: data.open_time,
+            price: data.price,
+            ch: data.ch,
+            ask: data.ask,
+            bid: data.bid,
+            price_gram_24k: data.price_gram_24k,
+            price_gram_22k: data.price_gram_22k,
+            price_gram_21k: data.price_gram_21k,
+            price_gram_20k: data.price_gram_20k,
+            price_gram_18k: data.price_gram_18k,
+            price_gram_16k: data.price_gram_16k,
+            price_gram_14k: data.price_gram_14k,
+            price_gram_10k: data.price_gram_10k,
+            nextExecution: new Date(Date.now() + 20 * 60 * 1000),
+        }, { upsert: true, new: true });
+        console.log("✅ Gold price updated");
+        return result;
+    }
+    catch (error) {
+        if (error.response.status === 403) {
+            console.log("❌ API key limit reached for key number : ", keys.current);
+            const newCurrent = (keys.current + 1) % keys.keys.length;
+            keys.current = newCurrent;
+            await keys.save();
+            return updateGoldPrice();
         }
-    });
-    const data = response.data;
-    // Rotate keys safely
-    const keysLength = keys.keys.length;
-    const newCurrent = (keys.current + 1) % keysLength;
-    keys.current = newCurrent;
-    await keys.save();
-    const result = await Metal.findOneAndUpdate({ symbol: data.symbol }, {
-        timestamp: data.timestamp,
-        metal: data.metal,
-        currency: data.currency,
-        exchange: data.exchange,
-        symbol: data.symbol,
-        open_time: data.open_time,
-        price: data.price,
-        ch: data.ch,
-        ask: data.ask,
-        bid: data.bid,
-        price_gram_24k: data.price_gram_24k,
-        price_gram_22k: data.price_gram_22k,
-        price_gram_21k: data.price_gram_21k,
-        price_gram_20k: data.price_gram_20k,
-        price_gram_18k: data.price_gram_18k,
-        price_gram_16k: data.price_gram_16k,
-        price_gram_14k: data.price_gram_14k,
-        price_gram_10k: data.price_gram_10k,
-        nextExecution: new Date(Date.now() + 20 * 60 * 1000),
-    }, { upsert: true, new: true });
-    console.log("✅ Gold price updated");
-    return result;
+    }
 }
 //# sourceMappingURL=updatePrices.js.map
